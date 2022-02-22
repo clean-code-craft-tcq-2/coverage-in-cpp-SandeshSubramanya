6dd73c8b20cc64f725ec1c80f0de01860989aef0
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-02-22 20:53:20 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -19,7 +19,7 @@ BreachType classifyTemperatureBreach(CoolingType coolingType, double temperature
 
 typedef enum {
   TO_CONTROLLER,
-  TO_EMAIL
+  TO_EMAIL,
   NUM_ALTERTARGETS  // stores total number of alert targets.
 } AlertTarget;
 
