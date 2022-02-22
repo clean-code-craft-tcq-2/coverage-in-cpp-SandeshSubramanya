b071e1de931f6d0f63263435339baf07892dd37f
SandeshSubramanya <91111862+SandeshSubramanya@users.noreply.github.com>
2022-02-22 20:02:22 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,26 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+TEST_CASE("Test infers breach accordingly to the limits") 
+{
+  // value, lower_limit, upper_limit
+  #define Lower_limit 20
+  #define Upper_limit 30
+  
+  REQUIRE(inferBreach(12, Lower_limit, Upper_limit) == TOO_LOW);
+  REQUIRE(inferBreach(20, Lower_limit, Upper_limit) == NORMAL);
+  REQUIRE(inferBreach(30, Lower_limit, Upper_limit) == NORMAL);
+  REQUIRE(inferBreach(31, Lower_limit, Upper_limit) == TOO_HIGH);
+}
+
+TEST_CASE("Test infers breach according to the limits set to corresponding CoolingType") 
+{  
+  for (int count=0 ; count < oVectorCoolingTypeInfo.size(); ++count)
+  {
+    int lower_limits = oVectorCoolingTypeInfo[count].m_lowerLimit;
+    int upper_limits = oVectorCoolingTypeInfo[count].m_upperLimit; 
+    REQUIRE(inferBreach((lower_limits-1), lower_limits, upper_limits) == TOO_LOW);
+    REQUIRE(inferBreach((upper_limits-lower_limits), lower_limits, upper_limits) == NORMAL);
+    REQUIRE(inferBreach((upper_limits+10), lower_limits, upper_limits) == TOO_HIGH);
+  }
 }
